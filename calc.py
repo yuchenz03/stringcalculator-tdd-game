07c6dd7ae923125181315c0a3313d7420f5fd54f
Yuchen Zhou <yuchen2006@outlook.com>
2026-06-10 10:13:23 -0400

    green commit, passing unknown numbers separated by new line tests

--- calc.py
+++ calc.py
@@ -4,6 +4,7 @@ def add(numbers):
     if numbers == "":
         return 0
     total = 0
+    numbers = numbers.replace("\n", ",")
     for num in numbers.split(","):
         total += int(num)
     return total
