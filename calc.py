b7b7fa4b6bd078d97d6d3cb423d71075a517dac4
Yuchen Zhou <yuchen2006@outlook.com>
2026-06-10 10:23:34 -0400

    green commit, completed custom delimiter requirement tests

--- calc.py
+++ calc.py
@@ -3,8 +3,15 @@
 def add(numbers):
     if numbers == "":
         return 0
-    total = 0
+    
     numbers = numbers.replace("\n", ",")
+    delim = ","
+    if numbers[0:2] == "//":
+        delim = numbers[2]
+        numbers = numbers[4:]
+
+    total = 0
+    numbers = numbers.replace(delim, ",")
     for num in numbers.split(","):
         total += int(num)
     return total
