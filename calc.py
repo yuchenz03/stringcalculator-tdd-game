dfcf8235e17bc05e5f1ea47de3b30d7cfb813a2d
Yuchen Zhou <yuchen2006@outlook.com>
2026-06-10 10:09:48 -0400

    green commit, passing unknown amount of numbers requirement comma separated

--- calc.py
+++ calc.py
@@ -3,7 +3,9 @@
 def add(numbers):
     if numbers == "":
         return 0
-    else:
-        return int(numbers)
+    total = 0
+    for num in numbers.split(","):
+        total += int(num)
+    return total
 
 
